2fcb60022bf307f8e5a5afcf8019c6c610f02031
gonzaloccastro <106605569+gonzaloccastro@users.noreply.github.com>
2023-02-21 12:17:12 -0300

    Entrega de Desafío

--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -10,9 +10,10 @@ const router = Router();
 router.get("/", async (req, res) => {
   const { email, password } = req.query;
 
-  if (!email || !password) {
-    res.render("login", {title: "Login" });
-  } else {
+    if(!email || !password || req.session == null || req.session.admin == false || req.session.admin ==undefined){
+      res.render("login", {title: "Login" });
+    }
+    else {
       try {
         const response = await userModel.find({
           email: email,
@@ -44,6 +45,26 @@ router.get("/", async (req, res) => {
 router.post("/", async (req, res) => {
     const { email, password } = req.body;
 
+});
+
+const auth = async (req,res,next) => {
+  console.log("auth", req.session.user);
+  if (await req.session?.admin) {
+    return next();
+  } else {
+    return res.status(401).send("error de autenticación");
+  }
+};
+
+router.get("/perfil", auth, async (req,res) => {
+
+  if (await req.session?.admin) {
+    console.log("Has entrado");
+    const userData = await userModel.findOne({email:req.session.user})
+    console.log("userData", userData);
+    const {first_name} = userData;
+    res.render("perfil",userData);
+  }
 });
 
 export default router;
